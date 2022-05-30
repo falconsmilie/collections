1a9c5e6a84966bff2a7eeae44c695cf3d03442cd
shanemartin <s.martin@metzler-vater.com>
2022-05-30 17:18:56 +0200

    create models

---
+++ src/Model/Food.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model;
+
+use App\Collection\CollectionInterface;
+
+class Food implements CollectionInterface
+{
+    protected array $items;
+
+    public function add()
+    {
+        // TODO: Implement add() method.
+    }
+
+    public function remove()
+    {
+        // TODO: Implement remove() method.
+    }
+
+    public function list()
+    {
+        // TODO: Implement list() method.
+    }
+
+    public function search()
+    {
+        // TODO: Implement search() method.
+    }
+}
---
+++ src/Model/Fruit.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class Fruit extends Food
+{
+
+}
