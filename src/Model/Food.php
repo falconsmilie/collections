b06609ee74e1f8abb562fc5f56bec4fe3e0626c1
shanemartin <s.martin@metzler-vater.com>
2022-05-31 16:28:14 +0200

    add convert unit method and some getters

--- src/Model/Food.php
+++ src/Model/Food.php
@@ -4,28 +4,40 @@ namespace App\Model;
 
 class Food extends Model
 {
-    private const UNIT_KILOGRAMS = 'kg';
-    private const UNIT_GRAMS = 'g';
+    public const UNIT_KILOGRAMS = 'kg';
+    public const UNIT_GRAMS = 'g';
 
     protected int $id;
     protected string $name;
     protected string $type;
-    protected int $quantity;
-    protected string $unit;
+    protected float $quantity;  // This is the weight
+    protected string $unit;     // This is the unit of weight measurement
 
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
-        $this->convertToGrams();
+        $this->convertUnit();
     }
 
-    public function convertToGrams()
+    public function convertUnit($unit = self::UNIT_GRAMS)
     {
-        if ($this->unit === self::UNIT_KILOGRAMS) {
-            $this->quantity *= 1000;
-            $this->unit = self::UNIT_GRAMS;
+        if ($this->unit !== $unit) {
+
+            $this->quantity = $unit === self::UNIT_GRAMS
+                ? $this->quantity *= 1000
+                : $this->quantity /= 1000;
+
+            $this->unit = $unit;
         }
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     public function setId(int $id): void
@@ -33,9 +45,29 @@ class Food extends Model
         $this->id = $id;
     }
 
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
     }
 
     public function setType(string $type): void
@@ -43,9 +75,9 @@ class Food extends Model
         $this->type = $type;
     }
 
-    public function setQuantity(int $quantity): void
+    public function getUnit(): string
     {
-        $this->quantity = $quantity;
+        return $this->unit;
     }
 
     public function setUnit(string $unit): void
