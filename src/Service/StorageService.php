808cd58a00bac7167a3deabce79ec11cd87f9611
shanemartin <s.martin@metzler-vater.com>
2022-05-31 13:13:20 +0200

    create fruit and vegetables members and collect them

--- src/Service/StorageService.php
+++ src/Service/StorageService.php
@@ -2,17 +2,37 @@
 
 namespace App\Service;
 
+
+use App\Collection\Collection;
+use App\Model\Fruit;
+use App\Model\Vegetable;
+
 class StorageService
 {
-    protected string $request;
+    private string $request;
+    private Collection $collection;
+    private Collection $fruit;
+    private Collection $vegetables;
 
-    public function __construct(string $request)
+    public function __construct(string $request, Collection $collection)
     {
         $this->request = $request;
+        $this->collection = $collection;
     }
 
     public function getRequest(): string
     {
         return $this->request;
+    }
+
+    public function process()
+    {
+        $this->fruit = $this->collection
+            ->filter(fn($item) => $item->type === 'fruit')
+            ->map(fn($item) => new Fruit((array)$item));
+
+        $this->vegetables = $this->collection
+            ->filter(fn($item) => $item->type === 'vegetable')
+            ->map(fn($item) => new Vegetable((array)$item));
     }
 }
--- tests/App/Service/StorageServiceTest.php
+++ tests/App/Service/StorageServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\App\Service;
 
+use App\Collection\Collection;
 use App\Service\StorageService;
 use PHPUnit\Framework\TestCase;
 
@@ -10,8 +11,9 @@ class StorageServiceTest extends TestCase
     public function testReceivingRequest(): void
     {
         $request = file_get_contents('request.json');
+        $items = json_decode($request);
 
-        $storageService = new StorageService($request);
+        $storageService = new StorageService($request, new Collection($items));
 
         $this->assertNotEmpty($storageService->getRequest());
         $this->assertIsString($storageService->getRequest());
